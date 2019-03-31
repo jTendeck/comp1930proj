3116aac75a6cfe702921642e0959e7c668d14f84
brandon vu <brandondvu@outlook.com>
2019-03-30 17:15:07 -0700

    made node host on default server 80

--- inSeason.js
+++ inSeason.js
@@ -7,9 +7,9 @@ app.get('/', function(req, res){
     res.sendFile('public/html/MainPage.html' , { root : __dirname});
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
     var host = server.address().address
     var port = server.address().port
 
-console.log('listening on port 8081');
+console.log('listening on port 80');
 })
